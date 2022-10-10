3ae62030903dc70940a04dc6256eca6933acc723
Sadhna kumari <sadhnakhairia1998@gmail.com>
2022-10-10 19:26:14 +0530

    new 1

--- login-form/src/App.js
+++ login-form/src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h1>
-        sadhna
-      </h1>
-      <h2>
-        hello world!!
-      </h2>
+      <h1>sadhna</h1>
+      <h2>hello world!!</h2>
+      <h2>this is the react project</h2>
     </div>
   );
 }
