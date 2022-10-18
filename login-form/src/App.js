16417df7fbc1808cf852aeef697d8aaf99129167
Sadhna kumari <sadhnakhairia1998@gmail.com>
2022-10-18 23:43:20 +0530

    new commit

--- login-form/src/App.js
+++ login-form/src/App.js
@@ -1,14 +1,1 @@
 import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return (
-    <div>
-      <h1>sadhna</h1>
-      <h2>hello world!!</h2>
-      <h2>this is the react project</h2>
-    </div>
-  );
-}
-
-export default App;
